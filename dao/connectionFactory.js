8a832faf921238d5363745b22787b6c1bf1183c6
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-04-21 21:22:00 -0500

    Actualizar a dependencia mysql2

--- dao/connectionFactory.js
+++ dao/connectionFactory.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
+/* eslint-disable linebreak-style */
+const mysql = require('mysql2');
 
 function connectionFactory() {
   return mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '20.163.172.38',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'equantom',
+    password: process.env.DB_PASSWORD || 'Yj?9khiu',
+    database: process.env.DB_NAME || 'libreshop',
   });
 }
+
 
 module.exports = () => connectionFactory;
